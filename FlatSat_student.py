637c5dc4878bab61e505d0ee968dbef07d616bfe
autofloccinaucinihilipilificator <125834149+autofloccinaucinihilipilificator@users.noreply.github.com>
2024-01-28 14:26:51 -0500

    Added config for picam2

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -10,10 +10,10 @@ The provided functions are only for reference, you do not need to use them.
 You will need to complete the take_photo() function and configure the VARIABLES section
 """
 
-#AUTHOR: AHS Physics Club
-#DATE: 22/01/24
+# AUTHOR: AHS Physics Club
+# DATE: 22/01/24
 
-#import libraries
+# import libraries
 import time
 import board
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
@@ -21,16 +21,25 @@ from adafruit_lis3mdl import LIS3MDL
 from git import Repo
 from picamera2 import Picamera2
 
-#VARIABLES - DO AT HOME
-THRESHOLD = 1      # Tweak this - Any desired value from the accelerometer
-REPO_PATH = ""     # Your github repo path: ex. /home/pi/FlatSatChallenge
-FOLDER_PATH = ""   # Your image folder path in your GitHub repo: ex. /Images
+# VARIABLES
+THRESHOLD = 1  # Tweak this - Any desired value from the accelerometer
+REPO_PATH = ""  # Your github repo path: ex. /home/pi/FlatSatChallenge
+FOLDER_PATH = "/images"  # Your image folder path in your GitHub repo: ex. /Images
 
-#imu and camera initialization
+# imu and camera initialization
 i2c = board.I2C()
 accel_gyro = LSM6DS(i2c)
 mag = LIS3MDL(i2c)
 picam2 = Picamera2()
+
+# is this stuff necessary?
+camera_config = picam2.create_still_configuration(
+    main={"size": (1920, 1080)},
+    lores={"size": (640, 480)},
+    display="lores"
+)
+picam2.configure(camera_config)
+picam2.start()
 
 
 def git_push():
@@ -65,13 +74,9 @@ def img_gen(name):
 
 
 def take_photo():
-    """
-    This function is NOT complete. Takes a photo when the FlatSat is shaken.
-    Replace psuedocode with your own code.
-    """
 
-    name = input("Enter your name (e.g.  RobertZ: ")
-  
+    name = input("Enter your name (e.g.  RobertZ): ")
+
     while True:
         accelx, accely, accelz = accel_gyro.acceleration
         print("accelx: ", accelx)
@@ -80,19 +85,19 @@ def take_photo():
 
         # if total accel above threshold
         if accelx ** 2 + accely ** 2 + accelz ** 2 > THRESHOLD ** 2:
-          time.sleep(5) # tweak this
-          file_name = img_gen(name)
-          picam2.capture_file(file_name)
-          git_push()
-          break
+            time.sleep(5)  # tweak this
+            file_name = img_gen(name)
+            picam2.capture_file(file_name)
+            git_push()
+            break
 
-        #CHECKS IF READINGS ARE ABOVE THRESHOLD
-            #PAUSE
-            #name = ""     #First Name, Last Initial  ex. MasonM # ???
-            #TAKE PHOTO
-            #PUSH PHOTO TO GITHUB
+        # CHECKS IF READINGS ARE ABOVE THRESHOLD
+        #   PAUSE
+        #   name = ""     #First Name, Last Initial  ex. MasonM # ???
+        #   TAKE PHOTO
+        #   PUSH PHOTO TO GITHUB
 
-        #PAUSE
+        # PAUSE
 
 
 def main():
