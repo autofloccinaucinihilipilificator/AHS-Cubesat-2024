179f17031b79defdb466e038f39631e1a6e89d11
Alex You <alexyou2006@outlook.com>
2024-02-08 20:04:07 -0500

    changed pathname

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -24,7 +24,7 @@ from picamera2 import Picamera2
 # VARIABLES
 THRESHOLD = 5  # Tweak this - Any desired value from the accelerometer
 REPO_PATH = "autofloccinaucinihilipilificator/AHS-Cubesat-2024"  # Your github repo path: ex. /home/pi/FlatSatChallenge
-FOLDER_PATH = "/images"  # Your image folder path in your GitHub repo: ex. /Images
+FOLDER_PATH = "image"  # Your image folder path in your GitHub repo: ex. /Images
 
 # imu and camera initialization
 i2c = board.I2C()
